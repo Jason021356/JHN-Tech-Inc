2109ed06f4f6789bf753c1f6a2e0c17203361034
Jason021365 <s0146765@go.edu.tw>
2023-11-21 15:02:34 +0800

    update files

--- pageanim.js
+++ pageanim.js
@@ -11,6 +11,10 @@ sc = document.getElementById("Screen");
 indev = document.getElementById("inputdevice");
 clpprdpg = document.getElementById("cellphoneNavBar");
 clpprdtag = document.querySelectorAll(".cellphonetag");
+td1 = document.getElementById("td1");
+td2 = document.getElementById("td2");
+td3 = document.getElementById("td3");
+mainmenubtn = document.getElementById("mainmenu");
 apppgcnt = 0;
 tds = "transition-duration: 0.2s;";
 mobilebl = false;
@@ -27,20 +31,20 @@ function defaultfn(...tag) {
 function PCalldivovstyle() {
     switch (apppgcnt) {
         case 1:
-            prdpg.style = "height:160px;background-color: white;visibility: unset;transition-duration: 0.2s;";
-            tctgtimeout = setTimeout(() => tctg.style = "position: relative;left: 100px;visibility: unset;color:black;", 200);
+            prdpg.style = "height:160px;visibility: unset;transition-duration: 0.2s;";
+            tctgtimeout = setTimeout(() => tctg.style = "position: relative;left: 100px;visibility: unset;color:white;", 200);
             tcicotimeout = setTimeout(() => tcico.style = "width: 40px;height: 40px;position: absolute;top: 15px;border-radius: 5px;left:50px;visibility:unset;opacity: 1.0;", 300);
-            gtrtgtimeout = setTimeout(() => gtrtg.style = "visibility:unset;left:100px;color:black;", 300);
-            vdctimeout = setTimeout(() => vdc.style = "visibility:unset;left:100px;top:80px;color:black;", 400);
-            aibottimeout = setTimeout(() => aibot.style = "position: absolute;left: 380px;color: black;visibility: unset;", 500);
-            anstimeout = setTimeout(() => ans.style = "position: absolute;left: 380px;color: black;visibility: unset;", 600);
+            gtrtgtimeout = setTimeout(() => gtrtg.style = "visibility:unset;left:100px;color:white;", 300);
+            vdctimeout = setTimeout(() => vdc.style = "visibility:unset;left:100px;top:80px;color:white;", 400);
+            aibottimeout = setTimeout(() => aibot.style = "position: absolute;left: 380px;color:white;visibility: unset;", 500);
+            anstimeout = setTimeout(() => ans.style = "position: absolute;left: 380px;color:white;visibility: unset;", 600);
             break;
         case 2:
-            prdpg.style = "height:300px;background-color: white;visibility: unset;transition-duration: 0.2s;";
-            NBtimeout = setTimeout(() => NB.style = "left: 6%;visibility: unset;color:black;", 200);
-            desktoptimeout = setTimeout(() => desktop.style = "left:6%;visibility:unset;color:black", 300);
-            sctimeout = setTimeout(() => sc.style = "left:6%;visibility:unset;color:black", 400);
-            indevtimeout = setTimeout(() => indev.style = "left:6%;visibility:unset;color:black", 500);
+            prdpg.style = "height:300px;visibility: unset;transition-duration: 0.2s;";
+            NBtimeout = setTimeout(() => NB.style = "left: 6%;visibility: unset;color:white;", 200);
+            desktoptimeout = setTimeout(() => desktop.style = "left:6%;visibility:unset;color:white;", 300);
+            sctimeout = setTimeout(() => sc.style = "left:6%;visibility:unset;color:white;", 400);
+            indevtimeout = setTimeout(() => indev.style = "left:6%;visibility:unset;color:white;", 500);
             break;
     }
 }
@@ -59,10 +63,18 @@ function PCalldivlvstyle() {
 }
 function Devicealldivanimstyle() {
     if (mobilebl == false) {
-        clpprdpg.style = "visibility:unset;width:100%;left:0;background-color: rgb(37, 37, 37);";
+        clpprdpg.style = "visibility:unset;width:100%;background-color: rgb(37, 37, 37);";
+        td1.style = "transform: rotate(45deg);";
+        td2.style = "visibility:hidden;background-color:rgba(0,0,0,0.0);top:5px;";
+        td3.style = "transform: rotate(-45deg);top:-5px;";
+        mainmenubtn.style = "background-color:rgba(37,37,37);top: 10px;right: 20px;"
         mobilebl = true;
     } else {
         clpprdpg.style = "";
+        td1.style = "";
+        td2.style = "top: 5px;";
+        td3.style = "top:10px";
+        mainmenubtn.style = "";
         mobilebl = false;
     }
 }
